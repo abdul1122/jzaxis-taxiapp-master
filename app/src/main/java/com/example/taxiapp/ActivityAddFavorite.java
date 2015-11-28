7fcf91600b536d8db40286cde36a62727faf2639
inbox-nisar <inbox.nisar@gmail.com>
2015-11-28 13:40:47 -0800

    commit 2nd time

--- app/src/main/java/com/example/taxiapp/ActivityAddFavorite.java
+++ app/src/main/java/com/example/taxiapp/ActivityAddFavorite.java
@@ -2,15 +2,28 @@ package com.example.taxiapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by hassanjamil on 2015-11-28.
  */
 public class ActivityAddFavorite extends Activity {
 
+    EditText etPlaceName,etLat,etLon;
+    Button btnSave,btnShowOnMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_favorite);
+        init();
+    }
+    private void init(){
+        etPlaceName=(EditText)findViewById(R.id.et_add_fav_placeName);
+        etLat=(EditText)findViewById(R.id.et_add_fav_lat);
+        etLon=(EditText)findViewById(R.id.et_add_fav_lon);
+        btnSave=(Button)findViewById(R.id.btn_add_fav_save);
+        btnShowOnMap=(Button)findViewById(R.id.btn_add_fav_showOnMap);
+
     }
 }
